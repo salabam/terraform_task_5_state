580868f95f12ab254f05dad5aae6a144101b980d
salabam <pavelgrom.chaban@gmail.com>
2026-02-14 02:48:29 +0200

    Solution

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.105.0"
+    }
+  }
+
+  backend "azurerm" {
+    storage_account_name = "boomtfstatestorage"
+    container_name       = "tfstate"
+    resource_group_name  = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc              = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
