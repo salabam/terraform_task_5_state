cc6883215dd0a49d6186437d1cbf2661bcce0d6f
salabam <pavelgrom.chaban@gmail.com>
2026-02-14 03:10:20 +0200

    Solution

--- main.tf
+++ main.tf
@@ -7,6 +7,8 @@ terraform {
   }
 
   backend "azurerm" {
+    # storage_account_name is hard-coded because Terraform backend
+    # does not support variables.
     storage_account_name = "boomtfstatestorage"
     container_name       = "tfstate"
     resource_group_name  = "tfstate"
